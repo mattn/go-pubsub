5679f79d849669ec08da6aa121aa13be976449b9
mattn <mattn.jp@gmail.com>
2013-12-19 20:14:20 +0900

    struct

--- example/example.go
+++ example/example.go
@@ -6,6 +6,10 @@ import (
 	"time"
 )
 
+type foo struct {
+	bar string
+}
+
 func main() {
 	mc := multichannel.New()
 	mc.Sub(func(i int) {
@@ -14,10 +18,14 @@ func main() {
 	mc.Sub(func(s string) {
 		fmt.Println("string subscriber: ", s)
 	})
+	mc.Sub(func(f *foo) {
+		fmt.Println("foo subscriber: ", f.bar)
+	})
 
 	mc.Pub(1)
 	mc.Pub("hello")
 	mc.Pub(2)
+	mc.Pub(&foo{"bar!"})
 
 	time.Sleep(5 * time.Second)
 }
