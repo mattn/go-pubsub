606d8b52d0483e4465ed7eab3c7fb3f306592a6b
mattn <mattn.jp@gmail.com>
2013-12-25 09:48:42 +0900

    Add chat example

---
+++ chat/chat.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"bufio"
+	"github.com/mattn/go-pubsub"
+	"log"
+	"net"
+	"strings"
+)
+
+func main() {
+	ps := pubsub.New()
+
+	l, err := net.Listen("tcp", ":5555")
+	if err != nil {
+		log.Fatal(err)
+	}
+	for {
+		c, err := l.Accept()
+		if err != nil {
+			log.Fatal(err)
+		}
+		go func(c net.Conn) {
+			buf := bufio.NewReader(c)
+			ps.Sub(func(t string) {
+				log.Println(t)
+				c.Write([]byte(t + "\n"))
+			})
+			log.Println("Subscribed", c.RemoteAddr().String())
+			for {
+				b, _, err := buf.ReadLine()
+				if err != nil {
+					log.Println("Closed", c.RemoteAddr().String())
+					break
+				}
+				ps.Pub(strings.TrimSpace(string(b)))
+			}
+			ps.Leave(nil)
+		}(c)
+	}
+}
